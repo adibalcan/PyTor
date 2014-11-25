bba2d21dc72ef8e330187dd689b0718cf9d9ede8
Adi Balcan <ady_1689@yahoo.com>
2014-11-25 19:55:52 +0200

    added new info in setup

--- setup.py
+++ setup.py
@@ -2,4 +2,12 @@ from distutils.core import setup
 setup(name='pytor',
       version='1.0',
       py_modules=['pytor'],
+      install_requires=[
+          'stem',
+          'requests',
+      ],
+      description='Python module for http requests by tor network',
+      keywords='tor proxy http requests',
+      author='Adrian Balcan',
+      author_email='adrian@thewebminer.com'
       )
