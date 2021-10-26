ef05927869fc1f939dd81383660c5769405b0cc1
Adi B <adrian@thewebminer.com>
2021-10-26 18:11:26 +0300

    Added Tor sample

---
+++ tor-sample.py
@@ -0,0 +1,11 @@
+from pytor import pytor
+
+# Automatically change the IP address if one of those strings are met in http response
+pytor.invalidStringList = ['Sorry, you\'re not allowed to access this page.', 'One more step']
+
+# Get source which contains your IP address :)
+print(pytor.getIp())
+
+# Change the identity and check again the IP address
+pytor.behaviour.changeIdentity()
+print(pytor.getIp())
